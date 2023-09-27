6819218217fa8d93dce0b2299f14da27e0a2d360
Ali Abrar <ali.abrar@obsidian.systems>
2023-09-27 14:05:49 -0400

    Only use wkwebview on ios

--- src/Obelisk/Frontend/IOS.hs
+++ src/Obelisk/Frontend/IOS.hs
@@ -1,9 +1,19 @@
+{-# Language CPP #-}
 module Obelisk.Frontend.IOS where
 
 import Data.ByteString (ByteString)
+#ifdef ios_HOST_OS
 import qualified Language.Javascript.JSaddle.WKWebView as JSaddle
+#endif
+
+type Webview =
+#ifdef ios_HOST_OS
+  JSaddle.WKWebview
+#else
+  ()
+#endif
 
 data IosConfig route = IosConfig
-  { _iosConfig_frontend :: Maybe JSaddle.WKWebView -> Frontend route
+  { _iosConfig_frontend :: Maybe WebView -> Frontend route
   , _iosConfig_initialHtml :: ByteString
   }
